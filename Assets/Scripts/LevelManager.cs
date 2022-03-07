95584315f3369e5874820c7435d82e66ea7755c5
Nicolas D'Heygere <nicolasdgr60@gmail.com>
2022-03-07 15:05:39 +0100

    post prossess check up platform is better

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -17,10 +17,11 @@ public class LevelManager : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		if (Application.platform == RuntimePlatform.IPhonePlayer)
-			InitTile(spritesLevelApple);
-		else
-			InitTile(spritesLevelAndroid);
+#if UNITY_IPHONE
+		InitTile(spritesLevelApple);
+#else
+		InitTile(spritesLevelAndroid);
+#endif
 		ShuffleTiles();
 		Placement();
 	}
